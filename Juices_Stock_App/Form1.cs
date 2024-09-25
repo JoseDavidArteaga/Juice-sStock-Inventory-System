1f19c84258e09e81245413bf83c6fcba71146ee9
Juan David Perdomo <juandavidperdomo10@gmail.com>
2024-09-25 08:17:44 -0500

    Cambio de text en el form

--- Juices_Stock_App/Form1.cs
+++ Juices_Stock_App/Form1.cs
@@ -16,5 +16,10 @@ namespace Juices_Stock_App
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
