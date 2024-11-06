5581b29ec80a9b0e388ccbb1f7d9f92e81e891f5
Brayan Gomes <gbrayansteven2@gmail.com>
2024-11-06 01:01:20 -0500

    Se agregaron las validaciones necesarias al momento de agregar un proveedor, campos llenos completamente, campo de teléfono con validaciones para un número válido (empezar por 3 y longitud máxima de 10, no deja ingresar caracteres diferentes a los digitos, tampoco dejar usar el comando ctrl v)

--- JuiceStockProject/Presentacion/Proveedores/frmProveedores_Agregar.Designer.cs
+++ JuiceStockProject/Presentacion/Proveedores/frmProveedores_Agregar.Designer.cs
@@ -85,6 +85,9 @@
             txbTelefono.Name = "txbTelefono";
             txbTelefono.Size = new Size(143, 27);
             txbTelefono.TabIndex = 20;
+            txbTelefono.TextChanged += txbTelefono_TextChanged;
+            txbTelefono.KeyDown += txbTelefono_KeyDown;
+            txbTelefono.KeyPress += txbTelefono_KeyPress;
             // 
             // txbNombre
             // 
@@ -93,6 +96,7 @@
             txbNombre.Name = "txbNombre";
             txbNombre.Size = new Size(164, 27);
             txbNombre.TabIndex = 19;
+            txbNombre.TextChanged += txbNombre_TextChanged;
             // 
             // label5
             // 
@@ -147,6 +151,7 @@
             txbDirección.Name = "txbDirección";
             txbDirección.Size = new Size(190, 27);
             txbDirección.TabIndex = 27;
+            txbDirección.TextChanged += txbDirección_TextChanged;
             // 
             // txbCorreo
             // 
@@ -155,6 +160,7 @@
             txbCorreo.Name = "txbCorreo";
             txbCorreo.Size = new Size(191, 27);
             txbCorreo.TabIndex = 26;
+            txbCorreo.TextChanged += txbCorreo_TextChanged;
             // 
             // frmProveedores_Agregar
             // 
